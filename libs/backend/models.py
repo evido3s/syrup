bdc789fd4c49d44ac3c291d379b6d2c6b44d9296
Michal Belica <devel@beli.sk>
2012-10-04 12:43:43 +0200

    data model updated for new linking system

--- libs/backend/models.py
+++ libs/backend/models.py
@@ -1,16 +1,25 @@
 from django.db import models
-from django.db.models import Sum
-from django.utils import timezone
 
 class Node(models.Model):
     typ = models.IntegerField()
     """node type: 0 - template, 1 - item, 2 - connector"""
+    links = models.ManyToManyField('Node', related_name = 'nodes')
+    linksup = models.ManyToManyField('Node', related_name = 'nodesdown')
+    linksdown = models.ManyToManyField('Node', related_name = 'nodesup')
+    templates = models.ManyToManyField('Node', related_name = 'instances')
+    primary_template = models.ForeignKey('Node', related_name = 'primary_instances', null = True)
 
     def __unicode__(self):
         if self.typ == 0:
-            return u"template %s" % ( self.get_name() )
+            try:
+                return u"template %s" % ( self.get_name() )
+            except:
+                return u"Template%d" % ( self.id )
         elif self.typ == 1:
-            return u"%s %s" % ( self.primary_template().get_name(), self.get_name() )
+            try:
+                return u"%s %s" % ( self.get_primary_template().get_name(), self.get_name() )
+            except:
+                return u"Item%d" % ( self.id )
         elif self.typ == 2:
             return u"Connector%d" % ( self.id )
 
@@ -21,19 +30,31 @@ class Node(models.Model):
         template.add_param(template, 'template_name', name, structural = True)
         return template
 
-    @staticmethod
-    def unlink(connector):
-        assert connector.typ == 2
-        for link in connector.connection.all():
-            link.delete()
-        connector.delete()
+    def delete_node(self):
+        self.delete()
 
-    def create_item(self):
+    def unlink(self, linked):
+        """Unlinks node from connector (could be called on either of them, with the other as argument)"""
+        if self.typ == 1 and linked.typ == 2:
+            node = self
+            connector = linked
+        elif self.typ == 2 and linked.typ == 1:
+            connector = self
+            node = linked
+        else: raise
+        try:
+            node.linksup.get(id = connector.id)
+            node.linksup.remove(connector)
+        except Node.DoesNotExist:
+            node.linksdown.get(id = connector.id)
+            node.linksdown.remove(connector)
+
+    def create_item(self, primary_value):
         """should be called on a template, creates instance of the template, returning the new node"""
-        # create new node
-        item = Node.objects.create(typ = 1)
-        # create link to primary template
-        item.template.create(template = self, primary = True)
+        assert self.typ == 0
+        primary_name = self.get_primary_param().name
+        item = self.primary_instances.create(typ = 1)
+        item.add_param(self, primary_name, primary_value, primary = True)
         return item
 
     def add_param(self, template, name, value, structural = False, primary = False):
@@ -72,42 +93,79 @@ class Node(models.Model):
                 params.append(param)
         return params
 
-    def list_linked(self):
+    def list_linked(self, direction):
+        """List linked items
+            direction - 0: straight, 1: up, -1: down
+            Returns dictionary of lists like { connector: [node1,node2...]... }
+        """
         linked = dict()
-        for link in self.link.all():
-            linked[link.connector] = link.connector.connection.exclude(node = self).get().node
+        if direction > 0:
+            linkset = self.linksup
+        elif direction < 0:
+            linkset = self.linksdown
+        else:
+            linkset = self.links
+        for connector in linkset.all():
+            if direction > 0:
+                nodeset = connector.nodesup
+            elif direction < 0:
+                nodeset = connector.nodesdown
+            else:
+                nodeset = connector.nodes
+            linked[connector] = nodeset.all()
         return linked
 
     def list_templates(self, incl_primary = True):
         templates = list()
         if incl_primary:
-            templates_queryset = self.template.all()
-        else:
-            templates_queryset = self.template.exclude(primary = True)
-        for link in templates_queryset:
-            templates.append(link.template)
+            templates.append(self.primary_template)
+        templates.extend(list(self.templates))
         return templates
 
-    def link_with(self, node):
-        """links with node, returning newly created connector"""
+    def link_with(self, node, direction):
+        """Links node (self) with another node
+            direction - 0: straight, 1: up, -1: down
+            returning newly created connector
+        """
         assert self.typ == 1 and node.typ == 1
-        link = self.link.create(connector = Node.objects.create(typ=2))
-        link.connector.connection.create(node = node)
-        return link.connector
+        if direction > 0:
+            connector = self.linksup.create(typ=2)
+            connector.nodesup.add(node)
+        elif direction < 0:
+            connector = self.linksdown.create(typ=2)
+            connector.nodesdown.add(node)
+        else:
+            connector = self.links.create(typ=2)
+            connector.nodes.add(node)
+        return connector
+
+    def link(self, node, direction):
+        """Links connector (self) with another node
+            direction - 0: straight, 1: up, -1: down
+        """
+        assert self.typ == 2 and node.typ == 1
+        if direction > 0:
+            self.nodesup.add(node)
+        elif direction < 0:
+            self.nodesdown.add(node)
+        else:
+            self.nodes.add(node)
 
     def link_template(self, template):
-        """links node with template"""
+        """Links node with template"""
         assert self.typ == 1 and template.typ == 0
-        link = self.template.create(template = template)
+        self.template.add(template)
 
     def unlink_template(self, template):
-        """unlinks node with template"""
+        """Unlinks node with template"""
         assert self.typ == 1 and template.typ == 0
-        # delete template link
-        TemplateLink.objects.filter(node = self, template = template).get().delete()
+        # check if template is linked
+        self.template.get(id = template.id)
         # delete parameters comming from that template
         for param in ParamStr.objects.filter(node = self, template = template):
             param.delete()
+        # delete template link
+        self.template.remove(template)
 
     def get_name(self):
         """Return object name (primary parameter of item or template_name of template)."""
@@ -117,14 +175,14 @@ class Node(models.Model):
             return self.paramstr_set.filter(primary = True).get().value
 
     def get_connector(self, node):
-        """Return QuerySet containing connector objects connecting self with node"""
+        """Return QuerySet containing connectors connecting self with node"""
         assert self.typ == 1 and node.typ == 1
-        connector = Node.objects.filter(connection__node = self).filter(connection__node = node)
-        return connector
+        connectors = Node.objects.filter(connection__node = self).filter(connection__node = node).all()
+        return connectors
 
-    def primary_template(self):
+    def get_primary_template(self):
         """Return primary template of node"""
-        return self.template.filter(primary = True).get().template
+        return self.primary_template
 
 class ParamStr(models.Model):
     node = models.ForeignKey(Node)
@@ -139,14 +197,14 @@ class ParamStr(models.Model):
                 u" (*)" if self.structural else u"",
                 u" (!)" if self.primary else u"")
 
-class Link(models.Model):
-    node = models.ForeignKey(Node, related_name = 'link')
-    connector = models.ForeignKey(Node, related_name = 'connection')
-
-    def __unicode__(self):
-        return u"Link%d node %d connector %d" % ( self.id, self.node.id, self.conn.id )
-
-class TemplateLink(models.Model):
-    node = models.ForeignKey(Node, related_name = 'template')
-    template = models.ForeignKey(Node, related_name = 'instance')
-    primary = models.BooleanField(default = False)
+#class Link(models.Model):
+#    node = models.ForeignKey(Node, related_name = 'link')
+#    connector = models.ForeignKey(Node, related_name = 'connection')
+#
+#    def __unicode__(self):
+#        return u"Link%d node %d connector %d" % ( self.id, self.node.id, self.conn.id )
+#
+#class TemplateLink(models.Model):
+#    node = models.ForeignKey(Node, related_name = 'template')
+#    template = models.ForeignKey(Node, related_name = 'instance')
+#    primary = models.BooleanField(default = False)
